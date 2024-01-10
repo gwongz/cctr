89fa925089e9fc8e6e9e07aad75550ec6303a532
Grace Wong <gwongz@gmail.com>
2024-01-10 11:47:59 +0000

    Decouple expander from source and target.

--- cctr.py
+++ cctr.py
@@ -39,20 +39,15 @@ class RangeExpander(Expander):
     Concrete class for subsitutions to be made for a range of characters.
     For example, "a-z"
     """
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+    def __init__(self, raw_value):
+        self.value = raw_value
 
     def expand(self):
-        pattern = self._expand_chars(self.source)
-        target = self._expand_chars(self.destination)
-        return pattern, target
-
-    def _expand_chars(self, specifier):
         """Expands characters in a range"""
-        list_of_chars = [chr(x) for x in range(ord(specifier[0]), ord(specifier[2]))]
+        list_of_chars = [chr(x) for x in range(ord(self.value[0]), ord(self.value[2]) + 1)]
         chars = ''.join(list_of_chars)
         return chars
+
 
 class ClassExpander(Expander):
     """
@@ -60,64 +55,64 @@ class ClassExpander(Expander):
     For example, "[:upper:]"
     """
 
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+    def __init__(self, raw_value):
+        self.value = raw_value
 
-    def _get_class_name(self, specifier):
+    def _get_class_name(self):
         pattern = r'[a-z]+'
-        match = re.search(pattern, specifier)
+        match = re.search(pattern, self.value)
         return match.group()
 
     def _is_valid_class(self, class_name):
         return class_name in CHARACTER_SPECIFIER_MAP
 
-    def _expand_specifier(self, specifier):
-        # expand lower to abcdefghijklmnopqrstuvwxyz
-        return CHARACTER_SPECIFIER_MAP.get(specifier)
-
     def expand(self):
-        source_class = self._get_class_name(self.source)
-        destination_class = self._get_class_name(self.destination)
-        if not self._is_valid_class(source_class):
-            raise ValueError("Unsupported source specifier")
-        if not self._is_valid_class(destination_class):
-            raise ValueError("Unsupported destination specifier")
+        class_name = self._get_class_name()
+        if not self._is_valid_class(class_name):
+            raise ValueError("Invald class specifier")
+        return CHARACTER_SPECIFIER_MAP.get(class_name)
 
-        pattern = self._expand_specifier(source_class)
-        target = self._expand_specifier(destination_class)
-
-        return pattern, target
 
 class SimpleExpander(Expander):
     """Basic character substitution"""
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+    def __init__(self, raw_value):
+        self.value = raw_value
 
     def expand(self):
-        if len(self.source) > len(self.destination):
-            padded_destination = self.destination * len(self.source)
-            return self.source, padded_destination
-        elif len(self.destination) > len(self.source):
-            limit = len(self.source)
-            return self.source, self.destination[:limit]
-        return self.source, self.destination
+        return self.value
+
 
 def substitute():
     """Copies the standard input to the standard output with substitution of selected characters """
     if is_class_specifier(constants.SOURCE):
-        expander = ClassExpander(constants.SOURCE, constants.DESTINATION)
+        source_expander = ClassExpander(constants.SOURCE)
     elif is_range_specifier(constants.SOURCE):
-        expander = RangeExpander(constants.SOURCE, constants.DESTINATION)
+        source_expander = RangeExpander(constants.SOURCE)
     else:
-        expander = SimpleExpander(constants.SOURCE, constants.DESTINATION)
-    pattern, target = expander.expand()
+        source_expander = SimpleExpander(constants.SOURCE)
+
+    if is_class_specifier(constants.DESTINATION):
+        destination_expander = ClassExpander(constants.DESTINATION)
+    elif is_range_specifier(constants.DESTINATION):
+        destination_expander = RangeExpander(constants.DESTINATION)
+    else:
+        destination_expander = SimpleExpander(constants.DESTINATION)
+
+    pattern = source_expander.expand()
+    target = destination_expander.expand()
+
+    if len(pattern) > len(target):
+        pad_count = len(pattern) - len(target)
+        target = f"{target}{target[-1] * pad_count}"
+    elif len(pattern) < len(target):
+        limit = len(pattern)
+        target = target[:limit]
+
     tr_map = str.maketrans(pattern, target)
 
     for line in sys.stdin:
         output = line.translate(tr_map)
-        print(output)
+        print(output, end="")
 
 if __name__ == '__main__':
     substitute()
